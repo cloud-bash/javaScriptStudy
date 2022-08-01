c13de1c41459a78ffb2c58a1d02af3d7d4d9d7d6
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2022-08-01 16:07:22 -0400

    Update bitwiseDemo.java

--- bitwiseDemo.java
+++ bitwiseDemo.java
@@ -3,24 +3,22 @@ class Main {
     Integer lowerlimit = 0;
     Integer upperlimit = 5;
     Integer operand = 9;
+    String obw = Integer.toBinaryString(operand);
     System.out.println("\nBitwise OR (|)");
     for (Integer i = lowerlimit; i < upperlimit; i++ ) {
       String ibw = Integer.toBinaryString(i);
-      String obw = Integer.toBinaryString(operand);
       String result = Integer.toBinaryString(i|operand);
       System.out.println(ibw + " | " + obw + " = " + result + " = " + (i | operand));
     }
     System.out.println("\nBitwise AND (&)");
     for (Integer i = lowerlimit; i < upperlimit; i++ ) {
       String ibw = Integer.toBinaryString(i);
-      String obw = Integer.toBinaryString(operand);
       String result = Integer.toBinaryString(i&operand);
         System.out.println(ibw + " ^ " + obw + " = " + result + " = " + (i & operand));
     }
     System.out.println("\nBitwise XOR (^)");
     for (Integer i = lowerlimit; i < upperlimit; i++ ) {
       String ibw = Integer.toBinaryString(i);
-      String obw = Integer.toBinaryString(operand);
       String result = Integer.toBinaryString(i^operand);
         System.out.println(ibw + " ^ " + obw + " = " + result + " = " + (i ^ operand));
     }

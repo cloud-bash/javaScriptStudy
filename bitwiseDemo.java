af8a1a8056ebba20c6e0eec22c4259e6c8edea61
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2022-08-01 07:34:38 -0400

    More descriptive and includes binary conversions

---
+++ bitwiseDemo.java
@@ -0,0 +1,28 @@
+class Main {
+  static void bitwise() {
+      Integer upperlimit = 5;
+      Integer operand = 2;
+    System.out.println("\n Bitwise XOR (^)");
+    for (Integer i = 0; i < upperlimit; i++ ) {
+      String ibw = Integer.toBinaryString(i);
+      String obw = Integer.toBinaryString(operand);
+        System.out.println(ibw + " ^ " + obw + " = " + (i ^ operand));
+    }
+    // System.out.println("\n|");
+    // for (Integer i = 0; i < upperlimit; i++ ) {
+    //     System.out.println(i | operand);
+    // }
+    // System.out.println("\n&");
+    // for (Integer i = 0; i < upperlimit; i++ ) {
+    //     System.out.println(i & operand);
+    // }    
+    // System.out.println("\n~");
+    // for (Integer i = 0; i < upperlimit; i++ ) {
+    //     System.out.println(~i);
+    // }
+}
+  
+  public static void main(String[] args) {
+  bitwise();
+    }
+  }
